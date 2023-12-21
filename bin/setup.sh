e645db5bb828c10587291bfd006fda168721aeb0
mihrafel <74379126+mihrafel@users.noreply.github.com>
2023-12-21 23:45:14 +0600

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -14,18 +14,19 @@ echo "Checking the Python version..."
 python3 --version
 sudo apt install python3.10-venv
 
+
 echo "Creating a Python virtual environment"
-python3 -m venv ~/backend-pics-venv
+python3 -m venv ~/backend-django-venv
 
 echo "Configuring the developer environment..."
 echo "# Backend Capstone Project additions" >> ~/.bashrc
-echo "export GITHUB_ACCOUNT=mihrafel" >> ~/.bashrc
+echo "export GITHUB_ACCOUNT=$GITHUB_ACCOUNT" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
 
 echo "Installing Python depenencies..."
-source /home/project/Back-End-Development-Pictures/backend-pics-venv/bin/activate && python3 -m pip install --upgrade pip wheel
-source /home/project/Back-End-Development-Pictures/backend-pics-venv/bin/activate && pip install -r requirements.txt
-echo "source /home/project/Back-End-Development-Pictures/backend-pics-venv/bin/activate" >> ~/.bashrc
+source ~/backend-pics-venv/bin/activate && python3 -m pip install --upgrade pip wheel
+source ~/backend-pics-venv/bin/activate && pip install -r requirements.txt
+echo "source ~/backend-pics-venv/bin/activate" >> ~/.bashrc
 
 echo "****************************************"
 echo " Capstone Environment Setup Complete"
